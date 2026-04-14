8ebf810b758a50902f68c0627ba9a23151c56835
cherlau <cherlau.prado@appfacilita.com>
2026-04-14 17:16:43 -0300

    Enhance Puppeteer launch arguments for improved stability and performance

--- server.js
+++ server.js
@@ -27,7 +27,14 @@ app.post('/generate-pdf', async (req, res) => {
   try {
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--no-first-run',
+        '--no-zygote',
+      ],
     });
 
     const page = await browser.newPage();
